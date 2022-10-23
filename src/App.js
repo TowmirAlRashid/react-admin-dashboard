726671d1abe42e23abc59e05ce7c9d7f3d47841a
TowmirAlRashid <alrashidtowmir420@gmail.com>
2022-10-23 13:48:37 +0600

    created the dashboard sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import Topbar from "./scenes/global/Topbar";
+import { Route, Routes } from "react-router-dom";
 import { ColorModeContext, useMode } from "./theme";
+
+import DashBoard from "./scenes/dashboard";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -10,8 +15,12 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar />
           <main className="content">
             <Topbar />
+            <Routes>
+              <Route path='/' element={ <DashBoard /> } />
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
