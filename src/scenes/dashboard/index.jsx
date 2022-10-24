10560fc869c47703dd5652d5d95ffa0fdc58d21c
TowmirAlRashid <alrashidtowmir420@gmail.com>
2022-10-24 09:50:23 +0600

    added the reusable Header and added to dashboard

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,8 +1,21 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import Header from '../../components/Header'
 
 const DashBoard = () => {
   return (
-    <div>DashBoard</div>
+    <Box m='20px'>
+      <Box
+        display='flex'
+        justifyContent='space-between'
+        alignItems='center'
+      >
+        <Header
+          title='DASHBOARD'
+          subtitle='Welcome to your personal Admin Dashboard!'
+        />
+      </Box>
+    </Box>
   )
 }
 
