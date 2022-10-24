61dfbb85695145eefec9bbf471fb4953bbf97f8e
TowmirAlRashid <alrashidtowmir420@gmail.com>
2022-10-24 13:07:33 +0600

    added the profile form page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import DashBoard from "./scenes/dashboard";
 import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
 import Invoices from "./scenes/invoices";
+import Form from "./scenes/profileForm";
+
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 
@@ -26,6 +28,13 @@ function App() {
               <Route path='/team' element={ <Team /> } />
               <Route path='/contacts' element={ <Contacts /> } />
               <Route path='/invoices' element={ <Invoices /> } />
+              <Route path='/form' element={ <Form /> } />
+              {/* <Route path="/bar" element={<Bar />} /> */}
+              {/* <Route path="/pie" element={<Pie />} /> */}
+              {/* <Route path="/line" element={<Line />} /> */}
+              {/* <Route path="/faq" element={<FAQ />} /> */}
+              {/* <Route path="/calendar" element={<Calendar />} /> */}
+              {/* <Route path="/geography" element={<Geography />} /> */}
             </Routes>
           </main>
         </div>
